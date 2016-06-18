dd543ba8ff086d3700df930dccb75b53bc9c6ddf
admin <q597756870@163.com>
2016-06-18 20:07:18 +0800

    first commit exportExcelTool

---
+++ ExcelExportTool/ExportExcel.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using OfficeOpenXml;
+
+namespace ExcelExportTool
+{
+    class ExportExcel
+    {
+        //xlsx path list
+        static List<string> pathLists = new List<string>();
+        static List<string> pathTransLists = new List<string>();
+
+        static ExportExcel _instance;
+
+        static List<string> filterFolderList = new List<string>();//过滤不需要导出的文件夹
+
+        static List<double> originIdList = new List<double>();
+        static List<double> translateIdList = new List<double>();
+        static List<double> translate2AddIdList = new List<double>();
+        static List<double> translate2DelIdList = new List<double>();
+
+        //static List<string> originIdList = new List<string>();
+        //static List<string> translateIdList = new List<string>();
+        //static List<string> translate2AddIdList = new List<string>();
+        //static List<string> translate2DelIdList = new List<string>();
+
+        public static ExportExcel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ExportExcel();
+                }
+                return _instance;
+            }
+        }
+
+        public static void Export(string rootPath, string[] transTypeList = null,string filterPath=null)
+        {
+            transTypeList = transTypeList == null ? new string[] { "ch" } : transTypeList;
+            rootPath = rootPath == "" ? @"E:\zzhx\trunk\data\ai" : rootPath;
+            filterFolderList.Clear();
+            if (filterPath != null && filterPath != "") {
+                filterFolderList.Add(filterPath);
+            }
+            pathLists.Clear();
+            TraverseFile(rootPath);
+
+            string pattern = @"[\u4e00-\u9fa5]+";
+            FileInfo excelFile;
+            for (int i = 0; i < pathLists.Count; i++)
+            {
+                //excel origin table
+                excelFile = new FileInfo(pathLists[i]);
+                if (!excelFile.Exists)
+                    continue;
+                //record the col num which to be exported;
+                List<int> collist = new List<int>();
+                using (ExcelPackage package = new ExcelPackage(excelFile))//每一个原excel
+                {
+                    //the first sheet
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+
+                    int colStart = worksheet.Dimension.Start.Column;  //工作区开始列
+                    int colEnd = worksheet.Dimension.End.Column;       //工作区结束列
+                    int rowStart = worksheet.Dimension.Start.Row;       //工作区开始行号
+                    int rowEnd = worksheet.Dimension.End.Row;       //工作区结束行号
+
+                    int workRow = 2;//默认工作行
+                    for (int row = rowStart; row <= 5; row++)
+                    {//给5因为有些表的字段名下面还加了些描述等中文字符，故多遍历几次，以保证进入正文
+                        for (int col = colStart; col <= colEnd; col++)
+                        {
+                            string text = worksheet.Cells[row, col].Text;
+                            if (text == "id" || text == "ID" || text.Contains("删"))
+                            {
+                                if (!collist.Contains(col))
+                                {
+                                    collist.Add(col);
+                                }
+                                //记录正文开始行
+                                if (text.Contains("删"))
+                                {
+                                    //删所在行的下一行为正文开始行
+                                    workRow = row + 1;
+                                }
+                                break;
+                            }
+
+                            MatchCollection mc = Regex.Matches(text, pattern, RegexOptions.IgnoreCase);
+                            if (mc.Count > 0)
+                            {
+                                if (!collist.Contains(col))
+                                {
+                                    collist.Add(col);
+                                }
+                            }
+                        }
+                    }
+                    //export
+                    string pathTranslate = pathLists[i];
+                    pathTranslate = pathTranslate.Substring(0, pathTranslate.Length - 5);//.xlsx
+                    pathTranslate += "_translate.xlsx";
+                    FileInfo newFile = new FileInfo(pathTranslate);
+                    if (newFile.Exists)
+                    {
+                        //newFile.Delete();
+                        using (ExcelPackage package2 = new ExcelPackage(newFile))//每一个translate excel
+                        {
+                            ExcelWorksheet workshee2 = package2.Workbook.Worksheets[1];
+                            int transColStart;
+                            int transColEnd;
+                            int transRowStart;
+                            int transRowEnd;
+                            //比对改动后的原表与translate表的id的不同后添加删除
+                            originIdList.Clear();
+                            translateIdList.Clear();
+                            translate2AddIdList.Clear();
+                            translate2DelIdList.Clear();
+                            for (int row = workRow; row <= rowEnd; row++)
+                            {
+                                if (worksheet.Cells[row, collist[0]].Value != null) {
+                                    originIdList.Add((double)worksheet.Cells[row, collist[0]].Value);
+                                    //originIdList.Add(worksheet.Cells[row, collist[0]].Value.ToString());
+
+                                }
+                            }
+
+                            transRowEnd = workshee2.Dimension.End.Row;
+                            for (int row = workRow; row <= transRowEnd; row++)
+                            {
+                                if (workshee2.Cells[row, collist[0]].Value != null) {
+                                    translateIdList.Add((double)workshee2.Cells[row, collist[0]].Value);
+                                    //translateIdList.Add(workshee2.Cells[row, collist[0]].Value.ToString());
+
+                                }
+                            }
+                            //比对origin 有 而translate无的
+                            for (int index = 0; index < originIdList.Count; index++)
+                            {
+                                if (!translateIdList.Contains(originIdList[index]))
+                                {
+                                    translate2AddIdList.Add(originIdList[index]);
+                                }
+                            }
+
+                            //比对origin 无 而translate有的
+                            for (int index = 0; index < translateIdList.Count; index++)
+                            {
+                                if (!originIdList.Contains(translateIdList[index]))
+                                {
+                                    translate2DelIdList.Add(translateIdList[index]);
+                                }
+                            }
+
+                            //对translate 表进行删改
+
+                            //string transId = "";
+                            double transId;
+                            //删除
+                            while (translate2DelIdList.Count > 0)
+                            {
+                                transColStart = workshee2.Dimension.Start.Column;
+                                transColEnd = workshee2.Dimension.End.Column;
+                                transRowStart = workRow;
+                                transRowEnd = workshee2.Dimension.End.Row;
+                                for (int row = transRowStart; row <= transRowEnd; row++)
+                                {
+                                    if (workshee2.Cells[row, transColStart].Value == null)
+                                        continue;
+                                    //transId = workshee2.Cells[row, transColStart].Value.ToString();
+                                    transId =(double)workshee2.Cells[row, transColStart].Value;
+
+                                    if (translate2DelIdList.Contains(transId))
+                                    {
+                                        //workshee2.Cells[row, transColStart, row, transColEnd].Value = null;
+                                        //ExcelRange range = workshee2.Cells[row, transColStart, row, transColEnd];
+                                        //range.Clear();
+                                        workshee2.DeleteRow(row);
+                                        translate2DelIdList.Remove(transId);
+                                    }
+                                }
+
+                            }
+                            //添加
+                            int curTranslateCol = 1;
+                            //string originId = "";
+                            double originId;
+                            while (translate2AddIdList.Count > 0)
+                            {
+                                transColStart = workshee2.Dimension.Start.Column;
+                                transColEnd = workshee2.Dimension.End.Column;
+                                transRowStart = workRow;
+                                transRowEnd = workshee2.Dimension.End.Row;
+
+                                for (int row = workRow; row <= rowEnd; row++)
+                                {//原表中的新数据所在行
+                                    if (worksheet.Cells[row, collist[0]].Value == null)
+                                        continue;
+                                    //originId = worksheet.Cells[row, collist[0]].Value.ToString();
+                                    originId = (double)worksheet.Cells[row, collist[0]].Value;
+                                    if (translate2AddIdList.Contains(originId))
+                                    {//找到原表中的新增数据
+                                        workshee2.InsertRow(row, 1);
+
+                                        for (int colindex = 0; colindex < collist.Count; colindex++)
+                                        {
+                                            //将原表里的内容加入
+                                            workshee2.Cells[row, curTranslateCol].Value = worksheet.Cells[row, collist[colindex]].Value;
+                                            if (colindex == 0)
+                                            {
+                                                curTranslateCol++;
+                                            }
+                                            else
+                                            {
+                                                curTranslateCol += transTypeList.Length + 1;
+                                            }
+                                        }
+                                        translate2AddIdList.Remove(originId);
+                                    }
+                                }
+                            }
+
+                            package2.Save();
+                        }
+                    }
+                    else
+                    {
+                        newFile = new FileInfo(pathTranslate);
+                        using (ExcelPackage package2 = new ExcelPackage(newFile))//每一个translate excel
+                        {
+                            ExcelWorksheet worksheet2 = package2.Workbook.Worksheets.Add("Sheet1");
+                            int curTranslateCol = 1;
+                            for (int colindex = 0; colindex < collist.Count; colindex++)
+                            {
+                                for (int rowindex = rowStart; rowindex <= rowEnd; rowindex++)
+                                {
+                                    //将原表里的内容加入
+                                    worksheet2.Cells[rowindex, curTranslateCol].Value = worksheet.Cells[rowindex, collist[colindex]].Value;
+                                    //根据翻译类型添加翻译字段
+                                    //id不需要翻译
+                                    if (colindex != 0)
+                                    {
+                                        for (int k = 0; k < transTypeList.Length; k++)
+                                        {
+                                            if (rowindex < workRow)
+                                            {
+                                                worksheet2.Cells[rowindex, curTranslateCol + 1 + k].Value = worksheet.Cells[rowindex, collist[colindex]].Value + "_" + transTypeList[k];
+                                            }
+                                            else
+                                            {
+                                                //worksheet2.Cells[rowindex, curTranslateCol + 1 + k].Value = worksheet.Cells[rowindex, collist[colindex]].Value;
+                                            }
+                                        }
+                                    }
+                                }
+                                //多种语言时
+                                if (colindex == 0)
+                                {
+                                    curTranslateCol++;
+                                }
+                                else
+                                {
+                                    curTranslateCol += transTypeList.Length + 1;
+                                }
+                            }
+                            package2.Save();
+                        };
+                    }
+
+                };
+            }
+            Console.Read();
+        }
+
+        static void TraverseFile(string rootPath)
+        {
+            DirectoryInfo folder = new DirectoryInfo(rootPath);
+
+            DirectoryInfo[] directoryinfos = folder.GetDirectories();
+            FileInfo[] fileinfos = folder.GetFiles();
+            if (directoryinfos.Length > 0)
+            {
+                //目录遍历
+                foreach (DirectoryInfo dir in directoryinfos)
+                {
+                    if (dir.Extension == ".meta")
+                        continue;
+                    //过滤
+                    if (filterFolderList.Count > 0)
+                    {
+                        bool isFilter = false;
+                        for (int i = 0; i < filterFolderList.Count; i++)
+                        {
+                            if (dir.FullName == filterFolderList[i])
+                            {
+                                isFilter = true;
+                                break;
+                            }
+                        }
+                        if (isFilter)
+                        {
+                            continue;
+                        }
+                    }
+
+                    TraverseFile(rootPath + "/" + dir.Name);
+                }
+            }
+            if (fileinfos.Length > 0)
+            {
+                //文件遍历
+                foreach (FileInfo dir in fileinfos)
+                {
+                    //文件查找匹配
+                    if (dir.Extension != ".xlsx")
+                        continue;
+                    if (dir.Name.Contains("translate"))
+                    {
+                        continue;
+                    }
+
+                    //pathLists.Add(dir.FullName + " " + dir.Name + " " + dir.DirectoryName);
+                    pathLists.Add(dir.FullName);
+                }
+            }
+        }
+
+        static void TraverseTranslateFile(string rootPath) {
+            DirectoryInfo folder = new DirectoryInfo(rootPath);
+
+            DirectoryInfo[] directoryinfos = folder.GetDirectories();
+            FileInfo[] fileinfos = folder.GetFiles();
+            if (directoryinfos.Length > 0)
+            {
+                //目录遍历
+                foreach (DirectoryInfo dir in directoryinfos)
+                {
+                    if (dir.Extension == ".meta")
+                        continue;
+                    TraverseTranslateFile(rootPath + "/" + dir.Name);
+                }
+            }
+            if (fileinfos.Length > 0)
+            {
+                //文件遍历
+                foreach (FileInfo dir in fileinfos)
+                {
+                    string pattern = @"[\s\S]+_translate[\s\S]";
+
+                    //文件查找匹配
+                    if (dir.Extension != ".xlsx")
+                        continue;
+                    //if (dir.Name.Contains("translate"))
+                    //{
+                    //    continue;
+                    //}
+                    MatchCollection mc = Regex.Matches(dir.Name, pattern, RegexOptions.IgnoreCase);
+                    if (mc.Count > 0)
+                    {
+                        pathTransLists.Add(dir.FullName);
+                        
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 删除翻译表
+        /// </summary>
+        /// <param name="rootPath"></param>
+        public static void DelTranslate(string rootPath) {
+            pathTransLists.Clear();
+            TraverseTranslateFile(rootPath);
+            foreach (var i in pathTransLists)
+            {
+                //Form1.ShowTips(i);
+                if (File.Exists(i)) {
+                    File.Delete(i);
+                }
+            }
+        }
+    }
+}
---
+++ ExcelExportTool/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelExportTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitContent();
+        }
+        void InitContent() {
+            this.MaximizeBox = false;
+
+            Screen scr = Screen.PrimaryScreen;
+            this.Size = new Size(scr.Bounds.Width/2,scr.Bounds.Height/2);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Excel翻译表导出";
+            //0
+            Label label1 = new Label();
+            label1.Location=new Point(24,8);
+            label1.Size = new Size(500, 20);
+            label1.Text="请选择要用于生成翻译表的原excel表所在目录";
+            this.Controls.Add(label1);
+
+            //1
+            TextBox txtbox = new TextBox();
+            txtbox.Location = new Point(24, 25);
+            txtbox.Size = new Size(500, 20);
+            txtbox.Text = @"E:\zzhx\trunk\data";
+            this.Controls.Add(txtbox);
+
+            //2
+            Button btn1 = new Button();
+            btn1.Location = new Point(530, 25);
+            btn1.Size = new Size(100, 20);
+            btn1.Text = "选择路径";
+            this.Controls.Add(btn1);
+            btn1.Click += btn1_Click;
+
+
+
+            //ListBox listbox = new ListBox();
+            //listbox.Location = new Point(24, 50);
+            //listbox.Size = new Size(300, 300);
+            //this.Controls.Add(listbox);
+            string[] translatetype = new string[] { "ch", "en", "ru", "ls", "ch", "en", "ru", "ls" };
+            //3
+            GroupBox gbox = new GroupBox();
+            gbox.Location = new Point(24, 100);
+            gbox.Size = new Size(300, 200);
+
+            Label label_bg = new Label();
+            label_bg.Location = new Point(24, 30);
+            label_bg.Size = new Size(200, 20);
+            label_bg.Text = "选择要添加的语言种类";
+            gbox.Controls.Add(label_bg);
+
+            int preIndex = 0;
+            int curIndex = 0;
+            for (int i = 0; i < translatetype.Length; i++) {
+                CheckBox cbox = new CheckBox();
+                curIndex++;
+                if (curIndex % 2 == 0)
+                    preIndex++;
+                cbox.Location = new Point(24+i%2*50, 50+preIndex*30);
+                cbox.Size = new Size(200, 30);
+                cbox.Text = translatetype[i];
+                gbox.Controls.Add(cbox);
+            }
+
+            this.Controls.Add(gbox);
+            //4
+            Button btn_del = new Button();
+            btn_del.Location = new Point(690, 330);
+            btn_del.Size = new Size(90, 20);
+            btn_del.Text = "删除翻译表";
+            this.Controls.Add(btn_del);
+            btn_del.Click += btn_translate_Del;
+            
+            //5
+            Button btn_translate = new Button();
+            btn_translate.Location = new Point(530, 150);
+            btn_translate.Size = new Size(100, 20);
+            btn_translate.Text = "导出Excel";
+            this.Controls.Add(btn_translate);
+            btn_translate.Click += btn_translate_Click;
+
+            //6
+            Label label_del = new Label();
+            label_del.Location = new Point(24, 310);
+            label_del.Size = new Size(500, 20);
+            label_del.Text = "请选择要删除翻译表起始目录";
+            this.Controls.Add(label_del);
+
+            //7
+            TextBox txtbox_del = new TextBox();
+            txtbox_del.Location = new Point(24, 330);
+            txtbox_del.Size = new Size(500, 20);
+            txtbox_del.Text = @"E:\zzhx\trunk\data";
+            this.Controls.Add(txtbox_del);
+
+            //8
+            Button btn_del_trans = new Button();
+            btn_del_trans.Location = new Point(560, 330);
+            btn_del_trans.Size = new Size(130, 20);
+            btn_del_trans.Text = "选择删除翻译表路径";
+            this.Controls.Add(btn_del_trans);
+            btn_del_trans.Click += btn_del_trans_Click;
+
+            //9
+            Label label_filter = new Label();
+            label_filter.Location = new Point(24, 50);
+            label_filter.Size = new Size(500, 20);
+            label_filter.Text = "请选择要过滤的目录";
+            this.Controls.Add(label_filter);
+
+            //10
+            TextBox txtbox_filter = new TextBox();
+            txtbox_filter.Location = new Point(24, 67);
+            txtbox_filter.Size = new Size(500, 20);
+            txtbox_filter.Text = @"";
+            this.Controls.Add(txtbox_filter);
+
+            //11
+            Button btn_filter = new Button();
+            btn_filter.Location = new Point(530, 67);
+            btn_filter.Size = new Size(130, 20);
+            btn_filter.Text = "选择过滤目录路径";
+            this.Controls.Add(btn_filter);
+            btn_filter.Click += btn_filter_Click;
+            
+        }
+
+        /// <summary>
+        /// 选择导出excel目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void btn1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            DialogResult result= openfile.ShowDialog();
+            if (result == DialogResult.OK) {
+                TextBox txtbox= this.Controls[1] as TextBox;
+                txtbox.Text = openfile.FileName;
+                int index= txtbox.Text.LastIndexOf(@"\");
+                txtbox.Text = txtbox.Text.Substring(0, index);
+                //ShowTips(txtbox.Text);
+            }
+        }
+
+        /// <summary>
+        /// 导出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void btn_translate_Click(object sender, EventArgs e) {
+            TextBox txtbox = this.Controls[1] as TextBox;
+            GroupBox gbox = this.Controls[3] as GroupBox;
+            List<string> translateType = new List<string>();
+            foreach (var item in gbox.Controls)
+            {
+                CheckBox cbox = item as CheckBox;
+                if (cbox == null)
+                    continue;
+                if (cbox.Checked)
+                {
+                    translateType.Add(cbox.Text);
+                }
+            }
+            TextBox txtbox_filter=this.Controls[10] as TextBox;
+            //string[] translateTypeArr = translateType.ToArray();
+            ExportExcel.Export(txtbox.Text, translateType.ToArray(), txtbox_filter.Text);
+            ShowTips("导出完成");
+        }
+
+        /// <summary>
+        /// 删除translate
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void btn_translate_Del(object sender, EventArgs e)
+        {
+            TextBox txtbox = this.Controls[7] as TextBox;
+            ExportExcel.DelTranslate(txtbox.Text);
+            ShowTips("删除完成");
+        }
+
+        /// <summary>
+        /// 选择删除translate目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void btn_del_trans_Click(object sender, EventArgs e) {
+            OpenFileDialog openfile = new OpenFileDialog();
+            DialogResult result = openfile.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                TextBox txtbox = this.Controls[7] as TextBox;
+                txtbox.Text = openfile.FileName;
+                int index = txtbox.Text.LastIndexOf(@"\");
+                txtbox.Text = txtbox.Text.Substring(0, index);
+            }
+        }
+
+
+
+        /// <summary>
+        /// 选择过滤目录路劲
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void btn_filter_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            DialogResult result = openfile.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                TextBox txtbox = this.Controls[10] as TextBox;
+                txtbox.Text = openfile.FileName;
+                int index = txtbox.Text.LastIndexOf(@"\");
+                txtbox.Text = txtbox.Text.Substring(0, index);
+            }
+        }
+
+
+        public static void ShowTips(string content) {
+            MessageBox.Show(
+                    content,"提示消息",MessageBoxButtons.OK
+                );
+        }
+    }
+}
